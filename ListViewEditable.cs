94e3a1792486a268c3b8e4a32baf0895cd955af7
Tyler H <cronotrigga@outlook.com>
2015-07-04 17:02:50 -0400

    Create ListViewEditable.cs

---
+++ ListViewEditable.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Windows.Forms;
+
+/// <summary>
+/// # Will create a listview component that has editiable sub items
+///  Ronny Frister gave an example of how to do this, i just added a few lines to make it more useable.
+/// </summary>
+class ListViewEditable : ListView
+{
+    public TextBox editbox = new TextBox();
+    public ListViewHitTestInfo hitInfo;
+
+    public ListViewEditable()
+    {
+        this.GridLines = true;
+        this.FullRowSelect = true;
+        this.DoubleBuffered = true;
+        editbox.Parent = this;
+        editbox.Hide();
+        editbox.LostFocus += new EventHandler(Editbox_LostFocus);
+        this.MouseDoubleClick += new MouseEventHandler(This_DoubleClick);
+    }
+
+    private void Editbox_LostFocus(object sender, EventArgs e)
+    {
+        hitInfo.SubItem.Text = editbox.Text;
+        editbox.Hide();
+    }
+
+    private void This_DoubleClick(object sender, MouseEventArgs e)
+    {
+        hitInfo = this.HitTest(e.X, e.Y);
+        editbox.Bounds = hitInfo.SubItem.Bounds;
+        editbox.Text = hitInfo.SubItem.Text;
+        editbox.Focus();
+        editbox.Show();
+    }
+}
+
